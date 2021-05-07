e8d5271aa579fefb00679fa32c73d813adba07c0
Windows 10 <Windows 10@DESKTOP-I8MKI0G>
2021-05-08 02:16:46 +0300

    initial commit

--- index.js
+++ index.js
@@ -23,4 +23,4 @@ app.get('/', async (req, res) => {
     })
 })
 
-app.listen(5000, () => console.log("Server is up and running....."));
+app.listen(process.env.PORT || 3000, () => console.log("Server is up and running....."));
